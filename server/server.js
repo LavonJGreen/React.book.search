6c722a0e565428a29ccb99559052603c34c4d80e
Lavon Green <lavon_green@yahoo.com>
2022-06-02 09:53:20 -0400

    updating read me

--- server/server.js
+++ server/server.js
@@ -39,8 +39,8 @@ app.get('*', (req, res) => {
 
 db.once('open', () => {
   app.listen(PORT, () => {
-    console.log(`📚 API server running on port ${PORT}!`);
-    console.log(`🚀 Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    console.log(` API server running on port ${PORT}!`);
+    console.log(` Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 
   });
 });
